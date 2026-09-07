8ac80e266608ee3130c8b02e102bd50256301025
SHASHANK DAREKAR <98036722+shashankdarekar07@users.noreply.github.com>
2026-09-07 23:48:51 +0530

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* 
+   This is a multi-line comment.
+   You can use this to explain larger blocks of code,
+   functions, or provide general program documentation.
+*/
+
+int main()
+{
+    // This is a single-line comment: Declaring an integer variable
+    int age; 
+    printf("enter age");
+    scanf("%d",&age);
+
+    // This is another single-line comment: Printing the value to the console
+    printf("Age: %d\n", age); 
+
+    return 0;
+}
